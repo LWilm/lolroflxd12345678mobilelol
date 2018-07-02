e13bd13935dcc5757a20c707496d4dd7558db4a5
lukas.wilm@gmx.de <twistK3!>
2018-07-02 14:04:52 +0200

    toolbar

--- app/src/main/java/com/example/d064036/greenwaste/MainActivity.java
+++ app/src/main/java/com/example/d064036/greenwaste/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.d064036.greenwaste;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
 }
