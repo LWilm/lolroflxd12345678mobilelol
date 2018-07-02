7a06e48ff9504204bf563e9e26eb51857b0b4a3c
lukas.wilm@gmx.de <twistK3!>
2018-07-02 23:23:40 +0200

    EventListener Toolbar

--- app/src/main/java/com/example/d064036/greenwaste/MainActivity.java
+++ app/src/main/java/com/example/d064036/greenwaste/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.d064036.greenwaste;
 
+import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -39,12 +40,13 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onDrawerOpened(View v) {
-                        super.onDrawerOpened(v);
+                        Intent intent = new Intent(this, EinstellungenActivity.class);
+                        startActivity(intent);
+//                        super.onDrawerOpened(v);
                     }
 
                 };
 
-       mDrawerLayout.addDrawerListener(DrawerLayout.DrawerListener DrawerListener);
 
     }
 
